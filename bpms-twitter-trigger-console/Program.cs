755944eac2365239ccccaa93cf1218bc66eb4f29
Samar Abbas - msft <samar.abbas@outlook.com>
2014-09-02 16:44:32 -0700

    Twitter trigger implmentation

---
+++ bpms-twitter-trigger-console/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Simple.Bpms.Triggers.Twitter.Console
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TwitterTrigger trigger = new TwitterTrigger(new string[] { "inqilab" }, false);
+            trigger.StartAsync();
+
+            Console.WriteLine("Press enter key to exit.");
+            Console.ReadLine();
+
+            trigger.Stop();
+        }
+    }
+}
